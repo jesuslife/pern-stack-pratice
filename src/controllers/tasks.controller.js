c5fefcd5286d74f094d029ba433b655b7841a8e3
gerardoap93 <gerardoap93@gmail.com>
2024-09-06 01:10:11 -0600

    mejora en manejo de errores en metodos post, get, update y delete en tasks.controller.js

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,12 +1,12 @@
 const pool = require('../db')
 
-const getAllTasks = async (req, res) => {
+const getAllTasks = async (req, res, next) => {
     try {
         const allTasks = await pool.query('SELECT * FROM task')
         console.log(allTasks)
         res.json(allTasks.rows)
     } catch(error){
-        res.json({error: error.message})
+        rnext(error);
     }
 
 //   res.send("retriven a list of tasks");
@@ -14,7 +14,7 @@ const getAllTasks = async (req, res) => {
   // res.json(result.rows[0])
 };
 
-const getSingleTask = async (req, res) => {
+const getSingleTask = async (req, res, next) => {
 
     try{
         const {id} = req.params
@@ -27,12 +27,12 @@ const getSingleTask = async (req, res) => {
     
         res.json(task.rows)
     }catch(error){
-        console.log(error.message);
+        rnext(error);
     }
 
 };
 
-const createTask = async (req, res) => {
+const createTask = async (req, res, next) => {
     const {title, description} = req.body;
 
     try{
@@ -41,42 +41,49 @@ const createTask = async (req, res) => {
         );
         res.send(result.rows[0]);
     }catch(error){
-        console.log(error.message);
-        res.json({error: error.message});
+        next(error);
+        // console.log(error.message);
+        // res.json({error: error.message});
+    }
+};
+
+const deleteTask = async (req, res, next) => {
+    
+    try {
+        const {id} = req.params
+        const result = await pool.query('DELETE FROM task WHERE id=$1',[id])
+        
+        if (result.rowCount === 0) 
+            return res.status(404).json({
+            mesage: "Task not found"
+        });
+        
+        return res.sendStatus(204);
+    }catch(error){
+        next(error)
     }
 
 
-
-    
 };
 
-const deleteTask = async (req, res) => {
-    
-    const {id} = req.params
-    const result = await pool.query('DELETE FROM task WHERE id=$1',[id])
-    
-    if (result.rowCount === 0) 
-        return res.status(404).json({
-        mesage: "Task not found"
-    });
-    
-    return res.sendStatus(204);
+const updateTask = async (req, res, next) => {
 
-};
-
-const updateTask = async (req, res) => {
-    const {id} = req.params
-    const {title, description} = req.body
-
-    const result = await pool.query(
-        "UPDATE task SET title = $1, description = $2 WHERE id = $3 RETURNING *",
-        [title, description, id]
-    );
-    if (result.rows.length === 0)
-        return res.status(404).json({
-            message: "Task not found"
-    });
-    return res.json(result.rows[0]);
+    try{
+        const {id} = req.params
+        const {title, description} = req.body
+    
+        const result = await pool.query(
+            "UPDATE task SET title = $1, description = $2 WHERE id = $3 RETURNING *",
+            [title, description, id]
+        );
+        if (result.rows.length === 0)
+            return res.status(404).json({
+                message: "Task not found"
+        });
+        return res.json(result.rows[0]);
+    } catch(error){
+        next(error)
+    }
 };
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -10,6 +10,11 @@ const app = express();
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(taskRoutes)
+app.use((err,req, res, next) =>{
+    return res.json({
+        message: err.message
+    })
+})
 app.listen(3000)
 
 console.log('server on port 3000')
