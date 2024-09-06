77068811941b4aa2fa17c6a33f8028463290bd42
gerardoap93 <gerardoap93@gmail.com>
2024-09-06 02:02:53 -0600

    material UI installation

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TaskList from "./components/TaskList";
 import TaskForm from "./components/TaskForm";
+import { Container } from "@mui/material";
+import Menu from "./components/Navbar";
 
 function App() {
   return (
     // BrowserRouter = voy a tener navegacion
     <BrowserRouter>
       {/* Voy a tener lista de rutas*/}
-      <Routes>
-        {/*// esta es mi ruta*/}
-        <Route path="/" element={<TaskList />} />
-        <Route path="/tasks/new" element={<TaskForm />} />
-      </Routes>
+      <Menu />
+      <Container>
+        <Routes>
+          {/*// esta es mi ruta*/}
+          <Route path="/" element={<TaskList />} />
+          <Route path="/tasks/new" element={<TaskForm />} />
+        </Routes>
+      </Container>
     </BrowserRouter>
   );
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,3 +1,10 @@
+import { Button } from "@mui/material";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      NavBar
+      <Button>Click Me</Button>
+    </div>
+  );
 }
